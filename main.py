da313977fce6f9427718dbd172edce9875986c39
James Alexander Harter <alexharter97@gmail.com>
2021-10-22 21:41:35 -0500

    Pseudo-code

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("Bruh")
+#user input scale note names
+
+#convert note names into pitch classes
+
+#generate complimentary set
+
+#add invariable pitch class(es)
+
+#convert new pitch class set to note names
+
+#call on "ChordID" library
